24bf687e1c19f175399bb25be2b8ac69531cad9e
Daniel235-web <samueledaniel235@gmail.com>
2023-10-17 20:37:30 +0100

    set the HerCarousel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
+
 
 const inter = Inter({ subsets: ['latin'] })
 const spaceGrotesk = Space_Grotesk({ 
@@ -23,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="max-w-10xl mx-auto">
-        
+          <Navbar />
           {children}
         </main>
       </body>
