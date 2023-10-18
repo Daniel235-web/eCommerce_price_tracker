e4b4f8766df4fb0fc47e0415d98ce5460ef1269a
Daniel235-web <samueledaniel235@gmail.com>
2023-10-18 14:46:03 +0100

    set the scrapping tool

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,7 +1,47 @@
 "use client"
+import { scrapeAmazonProduct } from '@/lib/scraper';
+import {FormEvent, useState} from 'react'
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if(
+      hostname.includes('amazon.com') ||
+       hostname.includes ('amazon.') || 
+       hostname.endsWith('amazon') 
+      ) {
+        return true;
+      } 
+  } catch (error) {
+    return false;
+  }
+  return false;
+}
 
 const Searchbar = () => {
-    const handleSubmit = () => {}
+  const  [searchPrompt, setsearchPrompt] = useState('');
+  const  [isLoading, setisLoading] = useState(false);
+
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      const isValidLink = isValidAmazonProductURL(searchPrompt);
+      if (!isValidLink) return alert ('Please provide a valid Amazon link')
+
+      try {
+        setisLoading(true);
+
+        //now let's scrap our first page
+        const product = await scrapeAmazonProduct(searchPrompt);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setisLoading(false);
+      }
+    }
 
   return (
     <form
@@ -10,11 +50,17 @@ const Searchbar = () => {
         >
             <input 
             type="text"
+            value={searchPrompt}
+            onChange={(e) => setsearchPrompt(e.target.value)}
             placeholder="Enter product link"
             className="searchbar-input"
             />
-            <button type="submit" className="searchbar-btn">
-                Search
+            <button
+             type="submit" 
+             className="searchbar-btn"
+             disabled = {searchPrompt === ''}
+             >
+                {isLoading ? 'Searching...' : 'Search'}
 
             </button>
 
